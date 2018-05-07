c3704d3176c152a6fcc581e7ffd9b31246bbf5df
nwaraya <nwaraya@gmail.com>
2018-05-06 23:32:38 -0300

    Agrego boton para deshabilitar un comercio

--- src/modules/comercio/DeshabilitarComercioModal.jsx
+++ src/modules/comercio/DeshabilitarComercioModal.jsx
@@ -3,10 +3,15 @@ import { connect } from 'react-redux'
 import { Row, Col, Button } from 'react-bootstrap'
 import { deshabilitarComercio } from './comercioReducer'
 import { CustomModalPregunta } from '../../utils/CustomModalPregunta'
+import { CustomFormField } from '../../utils/CustomFormField'
+import { FormControl } from 'react-bootstrap'
 
 export class HabilitarComercioModal extends React.Component {
   constructor() {
     super()
+    this.state = {
+      motivo: { error: false, mensaje: '' },
+    }
     this.abrirModal = this.abrirModal.bind(this)
     this.deshabilitar = this.deshabilitar.bind(this)
   }
@@ -27,6 +32,15 @@ export class HabilitarComercioModal extends React.Component {
         <Row key='0'>
           <Col md={12}>
             ¿Estás seguro de que deseas deshabilitar este comercio?
+          </Col>
+          <br/>
+          <Col md={12}>
+            <CustomFormField validationState={this.state.motivo.error ? 'error' : null}
+              validationMessage={this.state.motivo.mensaje} bsSize="small" controlId="motivo"
+              label="Motivo" inputComponent={
+                <FormControl defaultValue='' key="motivoInput" bsSize="small" rows="2" style={{resize: 'vertical'}}
+                  ref={motivoInput => { this.motivoInput = motivoInput }} type="text"  componentClass="textarea" ></FormControl>
+              } />
           </Col>
         </Row>
       </div>)
